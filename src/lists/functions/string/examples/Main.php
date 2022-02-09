8eb4d658bba992e4ade46f0bd3171b4f6fe27e59
HalloWelt42 <github@texeln.de>
2022-02-09 21:34:40 +0100

    feat: extend demos fluent, obj security

--- src/lists/functions/string/examples/Main.php
+++ src/lists/functions/string/examples/Main.php
@@ -6,9 +6,35 @@ class Main
 {
     public function __construct()
     {
+        $this->exampleMethodChaining();
+    }
+
+    public function exampleMethodChaining(){
+        $str = new StringType();
+
+        echo $str->set('Hallo')
+            ->add(' ')
+            ->add('World')
+            ->add('!')
+            ->add(PHP_EOL);
+    }
+
+
+    public function exampleStrinInString(){
+
         $str = new StringType('Test');
         $str->add(' gelungen!');
         $str_in_str = new StringType($str); // implizide Typenumwandlung vom StringType-Object $str
         print_r($str_in_str);
+
     }
+
+
+    public function exampleObjectSecurity(){
+        $str = new StringType();
+//        $str->test = 'exploid';
+//        print_r($str->test);
+    }
+
+
 }
